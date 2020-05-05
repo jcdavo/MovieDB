e7bbda1f3a2e3b51b1183925bde936eaa7438bf0
Davo <Davo@Davo-Mac.local>
2020-05-04 21:36:12 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// Tell Express to use body-parser
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// app.post("/page2", (req, res) => {
+//   var newUrl = req.body.url;
+//   console.log(newUrl);
+// });
+
+app.get("/", (req, res) => {
+  var h1Tittle = "Davo Movie Database";
+  var slogan = "Movie and Tv Show DataBase";
+  res.render("search", {
+    h1Tittle: h1Tittle,
+    paragraph: slogan,
+  });
+});
+
+// Compare function for year sorting
+function compare(a, b) {
+  const movieA = a.Year;
+  const movieB = b.Year;
+  let comparison = 0;
+  if (movieA > movieB) {
+    comparison = 1;
+  } else if (movieA < movieB) {
+    comparison = -1;
+  }
+  console.log(comparison);
+  return comparison * -1;
+}
+
+app.get("/results", (req, res) => {
+  var search = req.query.search;
+  var url = `http://www.omdbapi.com/?s=${search}&apikey=thewdb`;
+  var h1Tittle = "Search Results";
+  var paragraph = `Search results for ${search.toUpperCase()}`;
+  var totalResults = 0;
+  fetch(url)
+    .then((body) => body.json())
+    .then((data) => {
+      if (data.Response === "True") {
+        // Sort by year calling a compare function to the JSON
+        // data.Search.sort(compare);
+        // Single line to call a function
+        data.Search.sort((a, b) => (a.Year > b.Year ? -1 : 1));
+        totalResults = data.totalResults;
+        res.render("results", {
+          data: data,
+          h1Tittle: h1Tittle,
+          paragraph: paragraph,
+        });
+      } else {
+        res.render("results", {
+          data: data,
+          h1Tittle: h1Tittle,
+          paragraph: data.Error,
+        });
+      }
+    })
+    .catch((err) => {
+      console.log("Error!", err);
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
+  console.log("Now serving port 3000");
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Movie DataBase
+
+- This is a movie database running from the API from Open Movie DataBase
+
+- Implementation is part of a Web Development Course, with the key features being able to pull JSON data and work with API's
+
+- As part of the Web Development course, the project concluded as soon as we could retrieve data from the DB based on a search query.
+  Manipulate the JSON, and display the 10 object tittle returned as a list on HTML.
+  There is no styles applied to the entire project.
+
+- Eventually some styling was applied to make it more appealing
+  Grid layout for the movie tittles as well a poster image
+  An image is added if there is no poster available from the DB
+  Movies are displayed in realize order from new to old
+
+# Things to implement
+
+- Pagination, might have to create logic with a page variable to check if page is assigned
